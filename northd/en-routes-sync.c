ee071203badac0aba641a63f96448380f70af6b8
Felix Huettner <felix.huettner@stackit.cloud>
2024-09-10 09:24:42 +0200

    do not remove received routes, ovn-controller must handle them

--- northd/en-routes-sync.c
+++ northd/en-routes-sync.c
@@ -193,7 +193,10 @@ routes_table_sync(struct ovsdb_idl_txn *ovnsb_txn,
     }
 
     HMAP_FOR_EACH_POP (route_e, hmap_node, &sync_routes) {
-        if (route_e->stale) {
+        /* `receive` routes are added by ovn-controller we should only read but
+         * not remove them */
+        if (strcmp(route_e->sb_route->type, "receive") &&
+                route_e->stale) {
             sbrec_route_delete(route_e->sb_route);
         }
         route_erase_entry(route_e);
